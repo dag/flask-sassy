66eba23d7ac8c2104d3c1e0de6fa1329304b2f8c
Dag Odenhall <dag.odenhall@gmail.com>
2011-05-08 16:08:54 +0200

    pass @import tests

--- flaskext/sassy.py
+++ flaskext/sassy.py
@@ -1,12 +1,12 @@
 from os import path
 
-from pyscss.scss import Scss
+from pyscss import scss
 
 
 class Sassy(object):
 
     def init_app(self, app):
-        self.compiler = Scss()
+        self.compiler = scss.Scss()
         self.compiler.scss_opts.update(compress=False)
         self.app = app
         self.directory = path.join(app.root_path, 'stylesheets')
@@ -15,8 +15,14 @@ class Sassy(object):
 
     def compile(self, filename):
         with self.app.open_resource(path.join(self.directory, filename)) as f:
-            scss = f.read()
-        return self.compiler.compile(scss)
+            contents = f.read()
+        _old = scss.LOAD_PATHS
+        scss.LOAD_PATHS = ','.join([self.directory, scss.LOAD_PATHS])
+        try:
+            compiled = self.compiler.compile(contents)
+        finally:
+            scss.LOAD_PATHS = _old
+        return compiled
 
     def stylesheet(self, name):
         return self.app.response_class(self.compile(name + '.scss'),
